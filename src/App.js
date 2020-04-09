e8234a5f70699f04b7708040c093c814431e810a
sanjoyk <sanjoyksardar12@gmail.com>
2020-04-09 10:13:57 +0530

    jsx live compiler initila commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { PureComponent } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends PureComponent{
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '/* add your jsx here */',
+      output: "",
+      error: ""
+    }
+  }
+  update(e) {
+    let code = e.target.value;
+    console.log(code);
+    try {
+      console.log(window.Babel
+        .transform(code, { presets: ["es2015", "react"] })
+        .code)
+      this.setState({
+        output: window.Babel
+          .transform(code, { presets: ["es2015", "react"] })
+          .code,
+        error: ""
+      })
+    } catch (error) {
+      this.setState({ error: error.message })
+    }
+  }
+  render() {
+    console.log("output",this.state.output, typeof this.state.output);
+    return (
+      <div className="App">
+        <header className="header">
+          {this.state.error}
+        </header>
+        <div className="container">
+          <textarea
+            onChange={this.update.bind(this)}
+            defaultValue={this.state.input}
+          >
+          </textarea>
+          <pre>
+            {this.state.output}
+          </pre>
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
